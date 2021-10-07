e4f728b551ece8bc37484ac19d2fb9989cae77c2
mnr_dev <mack.n.ramirez@gmail.com>
2021-10-06 21:44:54 -0700

    a bit of security

--- src/Component/Books/Books.js
+++ src/Component/Books/Books.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+require('dotenv').config();
 const Books = () => {
   const [book, setBook] = useState([]);
   const [bookMeta, setBookMeta] = useState([]);
   console.log('book from books', book);
+  const api_key = process.env.REACT_APP_API_KEY;
 
   const fetchBooks = async () => {
     await axios
       .get(
-        `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=aTGWKgiHvl4TAtBqpUSvk9VXvEITqATg`
+        `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${api_key}`
       )
       .then((res) => setBook([...res.data.results.books]))
       .catch((error) => console.log('ERROR: ', error));
--- src/Component/MoviesGet.js
+++ src/Component/MoviesGet.js
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
 import Flick from './flick';
+require('dotenv').config();
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
   const [movieMeta, setMovieMeta] = useState([]);
+  const api_key = process.env.REACT_APP_API_KEY;
   //   console.log('Setting the State', movies);
   //   console.log('METADATA STATE', movieMeta);
 
   const fetchMovies = async () => {
     await axios
-      .get('')
+      .get(
+        `https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=${api_key}`
+      )
       .then((res) => {
         return setMovies([...res.data.results]), setMovieMeta([res.data]);
       })
