4256159d5dafada34b18686652733b8a8db5cb96
mnr_dev <mack.n.ramirez@gmail.com>
2021-10-25 21:56:56 -0700

    searchBar and nav accomodations

--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa';
+import SearchBar from './SearchBar';
 
 const Navbar = () => {
   return (
-    <nav className="contianer flex flex-col ">
+    <nav className="contianer flex flex-col md:flex-row justify-around">
       <div className="logo p-6 text-gray-800">
         <h1>MNR_BOOKS&MOVIES</h1>
       </div>
-      <div className="hamburger font-bold text-2xl">
+      <div className="hamburger font-bold text-2xl md:hidden">
         <FaBars />
       </div>
-      <div className="nav-links bg-gray-800 text-center ">
-        <div className="text-white">
+      <div className="nav-links bg-gray-800 flex items-center">
+        <div className="text-white p-5">
           <NavLink to="/">Home</NavLink>
         </div>
-        <div className="text-white">
+        <div className="text-white p-5">
           <NavLink to="/Books">Books</NavLink>
         </div>
       </div>
+      <SearchBar />
     </nav>
   );
 };
---
+++ src/Component/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return (
+    <form className="p-4 w-60 border-b-2 bg-gray-500">
+      <input
+        className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none focus:bg-gray-200"
+        type="text"
+        aria-label="Full name"
+        placeholder="Search"
+      />
+    </form>
+  );
+};
+
+export default SearchBar;
